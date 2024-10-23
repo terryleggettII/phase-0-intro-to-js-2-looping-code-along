5b9b783d9a7056e9fac632d43bf6ea42c6e6895e
Terry Leggett II <terryleggettjr@gmail.com>
2024-10-23 08:58:04 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const students = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(students) {
+    let messages = []; 
+    for (let i = 0; i < students.length; i++) {
+        const message = `Thank you, ${students[i]}, for the wonderful surprise gift!`;
+        console.log(message);
+        messages.push(message); 
+    }
+
+    return messages;
+}
+
+// function countDown() {
+//     let startingNumber = 11
+//     while (startingNumber > 0) {
+//         console.log(startingNumber--)
+//     }
+// }
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {  
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
